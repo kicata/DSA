6873333ddd62672b65cc43b812dd2f0a7c6f90c9
Kiril Asenov <kicata_bg@yahoo.com>
2016-03-16 14:58:56 +0200

    test

---
+++ HelloWordExample.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld  
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class HelloWorldExample
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                 Console.WriteLine("Test {0}",i);
+            }
+            Console.WriteLine("End of Test");
+        }
+    }
+}
